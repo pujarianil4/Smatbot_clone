f99bf495e3cbbf8c5660e5e8c892e6d62fe5ff7f
Bhargav <77038785+Bhargav1224@users.noreply.github.com>
2021-02-04 16:58:33 +0530

    Update try.js

--- pricing/try.js
+++ pricing/try.js
@@ -9,3 +9,16 @@ function Originalstarter() {
 }
 starter.addEventListener('mouseover',Colorstarter);
 starter.addEventListener('mouseleave',Originalstarter)
+
+//creating button for bottom 
+var trybtn=document.getElementById('Try2');
+function Colortrybtn() {
+    trybtn.style.background="white";
+    trybtn.style.color="black";
+}
+function Originaltrybtn() {
+    trybtn.style.background="#110858 ";
+    trybtn.style.color="white";
+}
+trybtn.addEventListener('mouseover',Colortrybtn);
+trybtn.addEventListener('mouseleave',Originaltrybtn)
