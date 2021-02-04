b1c232cb82a56956b7987e8b57247f570ce83436
Bhargav <77038785+Bhargav1224@users.noreply.github.com>
2021-02-04 16:59:03 +0530

    Update pro.js

--- pricing/pro.js
+++ pricing/pro.js
@@ -9,3 +9,16 @@ function OriginalPro() {
 }
 Pro.addEventListener('mouseover',ColorPro);
 Pro.addEventListener('mouseleave',OriginalPro);
+
+//creating botton for bottom pro
+var probtn=document.getElementById('pro-btn2');
+function Colorprobtn() {
+    probtn.style.background="white";
+    probtn.style.color="black";
+}
+function Originalprobtn() {
+    probtn.style.background="hotpink";
+    probtn.style.color="white";
+}
+probtn.addEventListener('mouseover',Colorprobtn);
+probtn.addEventListener('mouseleave',Originalprobtn);
