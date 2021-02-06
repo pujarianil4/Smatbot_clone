7ec16e11d2ca12b56a64d676df3054cb1b821ae9
Bhargav <77038785+Bhargav1224@users.noreply.github.com>
2021-02-06 07:16:38 +0530

    Update b3.js

--- pricing/b3.js
+++ pricing/b3.js
@@ -11,6 +11,19 @@ function OriginalColor() {
 b3.addEventListener('mouseover',changeColor);
 b3.addEventListener('mouseout',OriginalColor);
 
+var starpro=[1800,5500];
+var newprices=[1600,5000];
+
+
+function proEnter() {
+    if(starpro[0]&&starpro[1]!==(newprices[0]&&newprices[1])){
+       
+        document.querySelector('.amuual').innerHTML="Rs."+newprices[0]+" /month";
+        document.querySelector('.amuual2').textContent="Rs."+newprices[1]+" /month";
+    }
+}
+
+b3.addEventListener('click',proEnter);
 
 
 
